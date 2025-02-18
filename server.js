287d0f36758c9b427f00e722e767841ac126874a
Zahira Mohammad <zahira.mak@outlook.com>
2025-02-17 19:42:08 -0700

    Updated index.html

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 const swaggerOptions = {
     swaggerDefinition: {
